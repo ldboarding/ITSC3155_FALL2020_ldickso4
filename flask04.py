cbb135513364f4225ff1a8ad43402affd9625b87
Lucas Dickson <ldickso4@uncc.edu>
2020-11-12 21:00:33 -0500

    flask app #4 - updated queries to return one

--- flask04.py
+++ flask04.py
@@ -34,19 +34,19 @@ with app.app_context():
 @app.route('/index')
 def index():
     #a_user = {'name': 'Lucas', 'email': "ldickso4@uncc.edu"}
-    a_user =  db.session.query(User).filter_by(email='mogli@uncc.edu')
+    a_user =  db.session.query(User).filter_by(email='ldickso4@uncc.edu').one()
     return render_template('index.html', user = a_user)
 
 @app.route('/notes')
 def get_notes():
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='ldickso4@uncc.edu').one()
     my_notes = db.session.query(Note).all()
     return render_template('notes.html', notes = my_notes, user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
-    my_note = db.session.query(Note).filter_by(id=note_id)
+    a_user = db.session.query(User).filter_by(email='ldickso4@uncc.edu').one()
+    my_note = db.session.query(Note).filter_by(id=note_id).one()
     return render_template('note.html', note = my_note, user = a_user)
 
 @app.route('/notes/new', methods=['GET', 'POST'])
@@ -66,7 +66,7 @@ def new_note():
 
         return redirect(url_for('get_notes'))
     else: 
-        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+        a_user = db.session.query(User).filter_by(email='ldickso4@uncc.edu').one()
         return render_template('new.html', user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
