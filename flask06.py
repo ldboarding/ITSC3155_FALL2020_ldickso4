328201f59153db4ad171e9f3718087ceae135fa3
Lucas Dickson <ldickso4@uncc.edu>
2020-12-03 18:52:30 -0500

    adding and displaying comments funtionatily

--- flask06.py
+++ flask06.py
@@ -12,8 +12,8 @@ from flask import session
 from database import db
 from models import Note as Note
 from models import User as User
-from forms import RegisterForm
-from forms import LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)     # create an app
 
@@ -56,9 +56,12 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='ldickso4@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('note.html', note = my_note, user = a_user)
+    if session.get('user'):
+        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
+        form = CommentForm()
+        return render_template('note.html', note = my_note, user = session['user'], form=form)
+    else:  
+        return redirect(url_for('login'))
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -109,6 +112,23 @@ def delete_note(note_id):
         db.session.commit()
 
         return redirect(url_for('get_notes'))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
     else:
         return redirect(url_for('login'))
 
